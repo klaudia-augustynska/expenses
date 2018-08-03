da182a72f4f42b35be1ecf94d32544274c7b4f20
Klaudynka <klaudia.augustynska@gmail.com>
2018-08-03 18:45:08 +0200

    poprawka przeniesienie do folderu innego

--- Expenses/Expenses.TestApp/Converters/IloscStronVisibilityConverter.cs
+++ Expenses/Expenses.TestApp/Converters/IloscStronVisibilityConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace Expenses.TestApp
+namespace Expenses.TestApp.Converters
 {
     class IloscStronVisibilityConverter : IValueConverter
     {

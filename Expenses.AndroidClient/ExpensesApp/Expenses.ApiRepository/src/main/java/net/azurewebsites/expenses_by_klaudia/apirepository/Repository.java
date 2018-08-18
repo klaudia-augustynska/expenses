f5fbfa6a699fdedfc7d1c42420bc59afd22141de
Klaudynka <klaudia.augustynska@gmail.com>
2018-08-18 14:28:56 +0200

    zaczatek projektu android

---
+++ Expenses.AndroidClient/ExpensesApp/Expenses.ApiRepository/src/main/java/net/azurewebsites/expenses_by_klaudia/apirepository/Repository.java
@@ -0,0 +1,4 @@
+package net.azurewebsites.expenses_by_klaudia.apirepository;
+
+public class Repository {
+}
---
+++ Expenses.AndroidClient/ExpensesApp/Expenses.ApiRepository/src/test/java/JUnit5ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class JUnit5ExampleTest {
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+        assertEquals(2, 1 + 1);
+    }
+}
